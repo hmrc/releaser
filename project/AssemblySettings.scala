a00543bb458c920af3af8458613e88d8eb916d57
Duncan Crawford <duncan@noughtsandones.co.uk>
2016-10-27 22:09:56 +0100

    Refactor out the use of the trait Build
    Build has been deprecated in favour of using .sbt files in sbt version 0.13.12

---
+++ project/AssemblySettings.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2015 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+object AssemblySettings {
+
+  import sbt.Keys._
+  import sbt._
+  import sbtassembly.AssemblyKeys._
+  import sbtassembly._
+
+  def apply()= Seq(
+    assemblyJarName in assembly := "releaser.jar",
+    assemblyMergeStrategy in assembly := {
+      case PathList("org", "apache", "commons", "logging", xs@_*) => MergeStrategy.first
+      case PathList("play", "core", "server", xs@_*) => MergeStrategy.first
+      case x =>
+        val oldStrategy = (assemblyMergeStrategy in assembly).value
+        oldStrategy(x)
+    },
+    artifact in(Compile, assembly) := {
+      val art = (artifact in(Compile, assembly)).value
+      art.copy(`classifier` = Some("assembly"))
+    }
+  )
+}
